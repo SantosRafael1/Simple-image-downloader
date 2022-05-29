1652cfc54ff12fb99dab5342d66843061d2901c2
Rafael Santos <contactrafaelsantos@gmail.com>
2022-05-29 12:39:52 -0300

    back-end finalizado
    Adicionado a lógica para salvar a imagem localmente

--- src/main/java/com/rafael/imgdownloader/Controller.java
+++ src/main/java/com/rafael/imgdownloader/Controller.java
@@ -1,14 +1,33 @@
 package com.rafael.imgdownloader;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import javax.swing.JOptionPane;
 
 public class Controller {
+    public javafx.scene.control.Button button_download_img;
     @FXML
-    private Label welcomeText;
+    private TextField link_field;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void handleButtonClick(javafx.event.ActionEvent actionEvent) {
+        String linkStr = link_field.getText();
+
+        try {
+            URL url = new URL(linkStr);
+            BufferedImage img = ImageIO.read(url);
+            File file = new File("/home/rafa/Downloads/image.jpg"); //linux directory
+            ImageIO.write(img, "jpg", file);
+        } catch(IOException e) {
+            JOptionPane.showMessageDialog(null, "Error: " + e);
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.rafael.imgdownloader {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.rafael.imgdownloader to javafx.fxml;
